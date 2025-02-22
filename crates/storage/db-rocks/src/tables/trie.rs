a861182749dd425818ad72f9457163b1679dae7d
Abhinav Prakash <abhinav.prakash319@gmail.com>
2025-02-22 10:53:30 +0530

    fix::> Replaced H256 -> B256
    Signed-off-by: Abhinav Prakash <abhinav.prakash319@gmail.com>

--- crates/storage/db-rocks/src/tables/trie.rs
+++ crates/storage/db-rocks/src/tables/trie.rs
@@ -1,8 +1,11 @@
-//! Table definitions for trie data.
-use super::codecs::TrieNodeCodec;
-use reth_db_api::table::{Table, TableConfig};
-use reth_primitives::{Account, H256};
+use crate::tables::codecs::trie::TrieNodeCodec;
+use crate::tables::TableConfig;
+use alloy_primitives::B256;
+use reth_db_api::table::Table;
+use reth_primitives::Account;
 use rocksdb::{ColumnFamilyDescriptor, Options};
+
+use super::codecs::trie::StorageTrieKey;
 
 /// Table storing the trie nodes.
 #[derive(Debug)]
@@ -10,10 +13,10 @@ pub struct TrieTable;
 
 impl Table for TrieTable {
     const NAME: &'static str = "trie";
+    const DUPSORT: bool = false;
 
-    type Key = H256; // Node hash
+    type Key = B256; // Node hash
     type Value = Vec<u8>; // RLP encoded node data
-    type Config = TrieTableConfigs;
 }
 
 /// Table storing account trie nodes.
@@ -22,10 +25,10 @@ pub struct AccountTrieTable;
 
 impl Table for AccountTrieTable {
     const NAME: &'static str = "account_trie";
+    const DUPSORT: bool = false;
 
-    type Key = H256;
+    type Key = B256;
     type Value = Account;
-    type Config = TrieTableConfigs;
 }
 
 /// Table storing storage trie nodes.
@@ -34,25 +37,8 @@ pub struct StorageTrieTable;
 
 impl Table for StorageTrieTable {
     const NAME: &'static str = "storage_trie";
+    const DUPSORT: bool = false;
 
-    type Key = (H256, H256); // (Account Hash, Storage Hash)
-    type Value = H256; // Storage Value
-    type Config = TrieTableConfigs;
-}
-
-/// Defines configurations for trie tables.
-#[derive(Debug, Default)]
-pub struct TrieTableConfigs;
-
-impl TableConfig for TrieTableConfigs {
-    fn descriptor(&self) -> ColumnFamilyDescriptor {
-        let mut opts = Options::default();
-        // Configure for trie workload
-        opts.set_compression_type(rocksdb::DBCompressionType::Lz4);
-        opts.set_block_size(16 * 1024); // 16KB blocks
-        opts.set_write_buffer_size(64 * 1024 * 1024); // 64MB write buffer
-        opts.set_target_file_size_base(64 * 1024 * 1024); // 64MB target file size
-
-        ColumnFamilyDescriptor::new(Self::NAME, opts)
-    }
+    type Key = StorageTrieKey; // (Account Hash, Storage Hash)
+    type Value = B256; // Storage Value
 }
